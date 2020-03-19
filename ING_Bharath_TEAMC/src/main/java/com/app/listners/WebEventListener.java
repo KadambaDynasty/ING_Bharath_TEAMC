1bde3c82210c424376b9fd1decd51dd795364e97
KadambaDynasty <62336312+KadambaDynasty@users.noreply.github.com>
2020-03-19 14:49:44 +0530

    Implemented log

--- ING_Bharath_TEAMC/src/main/java/com/app/listners/WebEventListener.java
+++ ING_Bharath_TEAMC/src/main/java/com/app/listners/WebEventListener.java
@@ -1,6 +1,5 @@
 package com.app.listners;
-import java.io.IOException;
-
+import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.WebDriver;
@@ -11,48 +10,50 @@ import com.app.base.UITestBase;
 
 
 
-
+ 
 
 public class WebEventListener extends UITestBase implements WebDriverEventListener {
 
+	Logger log=Logger.getLogger(WebEventListener.class);
+	
 	public void beforeNavigateTo(String url, WebDriver driver) {
-		System.out.println("Before navigating to: '" + url + "'");
+		log.info("Before navigating to: '" + url + "'");
 	}
 
 	public void afterNavigateTo(String url, WebDriver driver) {
-		System.out.println("Navigated to:'" + url + "'");
+		log.info("Navigated to:'" + url + "'");
 	}
 
 	public void beforeChangeValueOf(WebElement element, WebDriver driver) {
-		System.out.println("Value of the:" + element.toString() + " before any changes made");
+		log.info("Value of the:" + element.toString() + " before any changes made");
 	}
 
 	public void afterChangeValueOf(WebElement element, WebDriver driver) {
-		System.out.println("Element value changed to: " + element.toString());
+		log.info("Element value changed to: " + element.toString());
 	}
 
 	public void beforeClickOn(WebElement element, WebDriver driver) {
-		System.out.println("Trying to click on: " + element.toString());
+		log.info("Trying to click on: " + element.toString());
 	}
 
 	public void afterClickOn(WebElement element, WebDriver driver) {
-		System.out.println("Clicked on: " + element.toString());
+		log.info("Clicked on: " + element.toString());
 	}
 
 	public void beforeNavigateBack(WebDriver driver) {
-		System.out.println("Navigating back to previous page");
+		log.info("Navigating back to previous page");
 	}
 
 	public void afterNavigateBack(WebDriver driver) {
-		System.out.println("Navigated back to previous page");
+		log.info("Navigated back to previous page");
 	}
 
 	public void beforeNavigateForward(WebDriver driver) {
-		System.out.println("Navigating forward to next page");
+		log.info("Navigating forward to next page");
 	}
 
 	public void afterNavigateForward(WebDriver driver) {
-		System.out.println("Navigated forward to next page");
+		log.info("Navigated forward to next page");
 	}
 
 	public void onException(Throwable error, WebDriver driver) {
@@ -60,16 +61,14 @@ public class WebEventListener extends UITestBase implements WebDriverEventListen
 	}
 
 	public void beforeFindBy(By by, WebElement element, WebDriver driver) {
-		System.out.println("Trying to find Element By : " + by.toString());
+		log.info("Trying to find Element By : " + by.toString());
 	}
 
 	public void afterFindBy(By by, WebElement element, WebDriver driver) {
-		System.out.println("Found Element By : " + by.toString());
+		log.info("Found Element By : " + by.toString());
 	}
 
-	/*
-	 * non overridden methods of WebListener class
-	 */
+	
 	public void beforeScript(String script, WebDriver driver) {
 	}
 
@@ -77,7 +76,7 @@ public class WebEventListener extends UITestBase implements WebDriverEventListen
 	}
 
 	public void beforeAlertAccept(WebDriver driver) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
